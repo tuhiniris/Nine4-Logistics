951fe3bccb5c757ef9f9c4bfcb6a0e41b8d24141
Tuhinadri Banerjee <tuhiniris@gmail.com>
2021-08-10 04:27:19 +0530

    Update

--- main/nextjs-blog/pages/login.js
+++ main/nextjs-blog/pages/login.js
@@ -1,6 +1,11 @@
 import React, {useState} from 'react';
 import Router from 'next/router';
 import cookie from 'js-cookie';
+import Head from 'next/head'
+import Link from 'next/link'
+import NextNprogress from 'nextjs-progressbar';
+import fetch from 'isomorphic-unfetch';
+import useSWR from 'swr';
 
 const Login = () => {
   const [loginError, setLoginError] = useState('');
@@ -34,25 +39,281 @@ const Login = () => {
         }
       });
   }
+
+
+
   return (
-    <form onSubmit={handleSubmit}>
-      <p>Login</p>
-      <input
-        name="email"
-        type="email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      <input
-        name="password"
-        type="password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <input type="submit" value="Submit" />
-      {loginError && <p style={{color: 'red'}}>{loginError}</p>}
-    </form>
+    
+
+    <div className="container">
+      
+      
+      <NextNprogress
+  color="orange"
+  startPosition={0.3}
+  stopDelayMs={200}
+  height={3}
+  showOnShallow={true}
+  showSpinner = {false}
+  options={{ easing: 'ease', speed: 500, showSpinner: true }}
+/>
+      
+      
+      
+      
+      
+      <Head>
+      <title>Nine4 Logistics - Sign In
+      </title>
+      <link rel="icon" href="/nine4-logo.png" />
+    </Head>
+    
+    <main>
+
+
+
+
+
+    <form onSubmit={handleSubmit} className="myform2">
+    <center><img src="https://cdn.dribbble.com/users/563824/screenshots/3633228/untitled-5.gif" className="logo2" alt="React"  /></center>
+        <input
+          name="email"
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)} />
+
+
+
+        <input
+          name="password"
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)} />
+        <input type="submit" value="Submit" />
+        {loginError && <p style={{ color: 'red' }}>{loginError}</p>}
+
+      </form>
+      </main>
+
+      <footer>
+          <a
+            href="https://reactjs.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Powered by React{' '}
+            <img src="/react-logo.svg" alt="React" className="logo" />
+          </a>
+        </footer>
+
+
+      
+      <style jsx>{`
+        
+        
+        
+        
+        
+        .container {
+          min-height: 100vh;
+          /* padding: 0 0.5rem; */
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;  
+        }
+
+
+        
+        .noselect {
+  -webkit-touch-callout: none; /* iOS Safari */
+    -webkit-user-select: none; /* Safari */
+     -khtml-user-select: none; /* Konqueror HTML */
+       -moz-user-select: none; /* Old versions of Firefox */
+        -ms-user-select: none; /* Internet Explorer/Edge */
+            user-select: none; /* Non-prefixed version, currently
+                                  supported by Chrome, Edge, Opera and Firefox */
+}
+        
+
+        main {
+          padding: 5rem 0;
+          flex: 1;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+        }
+
+        footer {
+          width: 100%;
+          height: 100px;
+          border-top: 1px solid #eaeaea;
+          display: flex;
+          background-color: black;
+          justify-content: center;
+          align-items: center;
+          color : white;
+          transition: 0.2s;
+        }
+
+        footer:hover {
+          background-color: #4267B2;
+          color : white;
+          font-weight: bold;
+        }
+
+        footer img {
+          margin-left: 0.5rem;
+        }
+
+        footer a {
+          display: flex;
+          justify-content: center;
+          align-items: center;
+        }
+
+        a {
+          color: inherit;
+          text-decoration: none;
+        }
+
+        .title a {
+          color: #0070f3;
+          text-decoration: none;
+          transition: 0.5s;
+        }
+
+        .title a:hover,
+        .title a:focus,
+        .title a:active {
+         // text-decoration: underline;
+          color : #4267B2;  
+        }
+
+        .title {
+          margin: 0;
+          line-height: 1.15;
+          font-size: 4rem;
+        }
+
+        .title,
+        .description {
+          text-align: center;
+        }
+
+        .description {
+          line-height: 1.5;
+          font-size: 1.5rem;
+        }
+
+        code {
+          background: #fafafa;
+          border-radius: 5px;
+          padding: 0.75rem;
+          font-size: 1.1rem;
+          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
+            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
+        }
+
+        .grid {
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          flex-wrap: wrap;
+
+          max-width: 800px;
+          margin-top: 3rem;
+        }
+
+        .card {
+          margin: 1rem;
+          flex-basis: 45%;
+          padding: 1.5rem;
+          text-align: left;
+          color: inherit;
+          text-decoration: none;
+          border: 1px solid #eaeaea;
+          border-radius: 10px;
+          transition: color 0.35s ease, border-color 0.35s ease;
+        }
+
+        .card:hover,
+        .card:focus,
+        .card:active {
+          color: #0070f3;
+          border-color: #0070f3;
+          background-color:white;
+          
+        }
+
+        .card h3 {
+          margin: 0 0 1rem 0;
+          font-size: 1.5rem;
+        }
+
+        .card p {
+          margin: 0;
+          font-size: 1.25rem;
+          line-height: 1.5;
+        }
+
+        .logo {
+          height: 1em;
+        }
+
+        .logo2 {
+          height: 15em;
+          padding-top : 0.1rem;
+          margin-top : -3em;
+        }
+
+        @media (max-width: 600px) {
+          .grid {
+            width: 100%;
+            flex-direction: column;
+          }
+        }
+      `}</style>
+
+      <style jsx global>{`
+        html,
+        body {
+          padding: 0;
+          margin: 0;
+          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
+            sans-serif;
+            scrollbar-width: thin;          /* "auto" or "thin" */
+            scrollbar-color: blue orange;   /* scroll thumb and track */
+            
+              -webkit-touch-callout: none; /* iOS Safari */
+                -webkit-user-select: none; /* Safari */
+                 -khtml-user-select: none; /* Konqueror HTML */
+                   -moz-user-select: none; /* Old versions of Firefox */
+                    -ms-user-select: none; /* Internet Explorer/Edge */
+                        user-select: none; /* Non-prefixed version, currently
+                                              supported by Chrome, Edge, Opera and Firefox */
+            
+
+            
+        }
+
+        * {
+          box-sizing: border-box;
+        }
+        
+        
+
+
+      `}</style>
+      </div>
   );
+
+
+
+
+  
 };
 
 export default Login;
